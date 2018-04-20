b8ada984f795081adc60561518496361a1f25aec
meganmuirhead <megan@s56.net>
2018-04-20 17:22:10 -0600

    working on ajax call and getting back my json

--- action.js
+++ action.js
@@ -1,13 +1,32 @@
-function loadCourseData() {
-    $.ajax(
-        'https://uxcobra.com/golfapi/course11819.txt',
-        {
-            method: "GET",
-            success: function (data, textStatus, jqXHR)
-            {
+$(function () {
 
-            }
-
+    var $courseData = $('#par1');
+    $.ajax({
+        type: 'GET',
+        url: 'https://uxcobra.com/golfapi/course11819.txt',
+        success: function (getCourses) {
+            // console.log('success', data)
+            $.each(getCourses, function (i, teeBoxes) {
+                $courseData.append('<div> 1: '+ holes.teeBoxes.par +'</div>');
+            });
         }
-    );
-}
+
+    });
+    
+});
+
+
+// $.ajax(
+//     'https://uxcobra.com/golfapi/course11819.txt',
+//     {
+//         method: "GET",
+//         success: function (data, textStatus, jqXHR)
+//         {
+//
+//         }
+//
+//     }
+// );
+
+
+// loadCourseData
