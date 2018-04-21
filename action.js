784cd357cd5d6cd7bee2be9f0b6bd7083caeeea5
meganmuirhead <megan@s56.net>
2018-04-20 20:17:40 -0600

    working on ajax call still

--- action.js
+++ action.js
@@ -1,19 +1,19 @@
-$(function () {
-
-    var $courseData = $('#par1');
-    $.ajax({
-        type: 'GET',
-        url: 'https://uxcobra.com/golfapi/course11819.txt',
-        success: function (getCourses) {
-            // console.log('success', data)
-            $.each(getCourses, function (i, teeBoxes) {
-                $courseData.append('<div> 1: '+ holes.teeBoxes.par +'</div>');
-            });
-        }
-
-    });
-    
-});
+// $(function () {
+//
+//     var $courseData = $('#par1');
+//     $.ajax({
+//         type: 'GET',
+//         url: 'https://uxcobra.com/golfapi/course11819.txt',
+//         success: function (getCourses) {
+//             // console.log('success', data)
+//             $.each(getCourses, function (i, teeBoxes) {
+//                 $courseData.append('<div> 1: '+ holes.teeBoxes.par +'</div>');
+//             });
+//         }
+//
+//     });
+//
+// });
 
 
 // $.ajax(
@@ -29,4 +29,28 @@ $(function () {
 // );
 
 
-// loadCourseData
+loadCourseData
+
+var loadCourseData = new XMLHttpRequest();
+loadCourseData.open('GET', 'https://uxcobra.com/golfapi/course11819.txt');
+
+loadCourseData.onload = function(){
+
+var ourData = JSON.parse(loadCourseData.responseText);
+    console.log(ourData.data.holes);
+
+
+
+};
+loadCourseData.send();
+
+// var ourRequest = new XMLHttpRequest();
+// ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+//
+// ourRequest.onload = function(){
+//     var ourData = JSON.parse(ourRequest.responseText);
+//
+//     console.log(ourData[0].name);
+//
+// };
+// ourRequest.send();
